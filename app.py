96deeb1db69c8fd6aa1cb6dfbc3f5f3077f66415
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-03-04 15:08:17 +0200

    Додано тести

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ def gcd(n, m):
     while m % n:  # While `n` doesn't divide into `m`:
         n, m = m % n, n  # update the values of `n` amd `m`.
     return n
+
+
+def test_gcd(n, m):
+    d = gcd(n, m)
+
+    assert d > 0  # 1) `d` is positive
+    assert n % d == 0  # 2) `d` divides `n`
+    assert m % d == 0  # 3) `d` divides `m`
+
+    # 4) no other number larger than `d` divides both `n` and `m`
+    for i in range(d + 1, min(n, m)):
+        assert (n % i) or (m % i)
